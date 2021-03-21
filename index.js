a63941f277072d18cf9ba60b9cec0e4d45715de5
Tamara Jarvis <tamara.jarvis17@icloud.com>
2021-03-21 10:22:53 -0400

    commit tree 58a9f0b274009122865c5b2f9ce518e2e456818f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(driver, match) {
+    return driver.filter( nameMatch =>
+        nameMatch.toLowerCase() === match.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(driver, testString) {
+    return driver.filter( nameMatch =>
+        nameMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(driver, matchName) {
+    return driver.filter( record => record.name === matchName)
+  }
